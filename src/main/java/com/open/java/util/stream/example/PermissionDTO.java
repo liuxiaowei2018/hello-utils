b6219f7d92b62fdad00816582d76ff7c0a39cb04
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-10-17 11:56:49 +0800

    style: stream流处理 递归数据 用例

---
+++ src/main/java/com/open/java/util/stream/example/PermissionDTO.java
@@ -0,0 +1,79 @@
+package com.open.java.util.stream.example;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年10月17日 11:44
+ * @Description
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PermissionDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    private Integer permissionId;
+
+    /**
+     * 权限类型（1：菜单权限；2：操作权限）
+     */
+    private Integer permissionType;
+
+    /**
+     * 父级id，顶级为0
+     */
+    private Integer parentId;
+
+    /**
+     * 菜单id
+     */
+    private Integer menuId;
+
+    /**
+     * 操作
+     */
+    private String operate;
+
+    /**
+     * 操作编码
+     */
+    private String code;
+
+    /**
+     * 显示名称
+     */
+    private String displayValue;
+
+    /**
+     * 启用状态：0，未启用；1，已启用
+     */
+    private Integer status;
+
+    /**
+     * 排序
+     */
+    private Integer sort;
+
+    /**
+     * 子权限
+     */
+    private List<PermissionDTO> children;
+}
---
+++ src/main/java/com/open/java/util/stream/example/RegressionTest.java
@@ -0,0 +1,43 @@
+package com.open.java.util.stream.example;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年10月17日 11:43
+ * @Description 处理权限数据-递归算法
+ */
+@Slf4j
+public class RegressionTest {
+
+    public static void main(String[] args) {
+        String tempPath = "D:/permission.json";
+        JSONObject jsonObject = JSONUtil.readJSONObject(new File(tempPath), Charset.defaultCharset());
+
+        long start = System.currentTimeMillis();
+        String json = JSONUtil.toJsonStr(jsonObject.get("data"));
+        List<PermissionDTO> permissionList = JSONUtil.toList(json, PermissionDTO.class);
+        if (CollUtil.isNotEmpty(permissionList)) {
+            List<PermissionDTO> result = permissionList.stream().filter(p -> p.getParentId() == 0)
+                    .peek((p) -> p.setChildren(getChildren(p, permissionList)))
+                    .collect(Collectors.toList());
+            log.info(JSONUtil.toJsonStr(result));
+        }
+
+        System.out.println(System.currentTimeMillis()-start);
+    }
+
+    private static List<PermissionDTO> getChildren(PermissionDTO dto, List<PermissionDTO> list) {
+        return list.stream()
+                .filter(p -> dto.getPermissionId().equals(p.getParentId()))
+                .peek(p1 -> p1.setChildren(getChildren(p1, list))).collect(Collectors.toList());
+    }
+}
